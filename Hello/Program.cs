128eeab39384d75485e8216447920c4cfa281765
SphepheloN <SphepheloN@ZAZN-BET-0B725B>
2023-11-15 10:09:36 +0200

    First C# Program Print Hello World to Console

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,2 +1,8 @@
 ﻿
 
+string message = "Hello World";
+// string is a data type
+// message is a variable
+// 'Hello World' is a value of the variable
+Console.WriteLine(message);
+// method to display value on Console
